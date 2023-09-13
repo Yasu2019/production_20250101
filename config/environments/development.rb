ef418dd39cf3d7e6a92839172e3b6876e1f9f2c1
Yasu2019 <48149403+Yasu2019@users.noreply.github.com>
2023-09-13 15:12:22 +0900

    Change Mitsui Open IP address setting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
   #config.web_console.whitelisted_ips = %w(0.0.0.0/0 ::/0)
 
   #ミツイ精密社内からだけのアクセスを許可する場合
-  config.web_console.whitelisted_ips = ['192.168.5.0/24', '8.8.8.8']
+  config.web_console.whitelisted_ips = ['180.11.97.245']
 
   #自宅からだけのアクセスを許可する場合
 

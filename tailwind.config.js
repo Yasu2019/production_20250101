70b14fa07b72b89cecf9e6d062b63e7f52278be1
Yasu2019 <48149403+Yasu2019@users.noreply.github.com>
2023-11-11 22:11:11 +0900

    Revise tailwindscss.config.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
+
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
+  // ここに他の設定を追加
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  // 例えば、デフォルトテーマを拡張する設定
+  extend: {
+    fontFamily: {
+      sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+    },
+  },
 }

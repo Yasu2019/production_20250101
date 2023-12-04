adedd97ab29fff90ed53d1a4ceb452615c192664
Yasu2019 <48149403+Yasu2019@users.noreply.github.com>
2023-12-04 15:30:47 +0900

    Revise tailwindcss

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -6,32 +6,23 @@ const plugin = require("tailwindcss/plugin");
 
 
 module.exports = {
-  content: [
-    //'./public/*.html',
-    '../myapp/public/*.html',
+  //'./public/*.html',
+  '../myapp/public/*.html',
 
-    //'./app/helpers/**/*.rb',
-    '../myapp/app/helpers/*.rb',
-   
+  //'./app/helpers/**/*.rb',
+  '../myapp/app/helpers/*.rb',
 
-    //'./app/javascript/**/*.js',
-    '../myapp/app/javascript/**/*.js',
+  //'./app/javascript/**/*.js',
+  '../myapp/app/javascript/**/*.js',
 
+  //'./app/javascript/*.js',
+  '../myapp/app/javascript/*.js',
 
-    //'./app/javascript/*.js',
-    '../myapp/app/javascript/*.js',
-
-
-
-    //'./app/views/**/*.{erb,haml,html,slim}'
-    //Tailwind CSS for RailsがDockerコンテナ内で反映されないときの対処法。purge設定に"html.erb"を含めていないことが原因
-    //https://qiita.com/hajsu00/items/d7f2c7f382124d6e32ef
-
-
-    '../myapp/app/views/**/*.{html.erb,erb,haml,html,slim}',
-
-    
-
+  //'./app/views/**/*.{erb,haml,html,slim}'
+  //Tailwind CSS for RailsがDockerコンテナ内で反映されないときの対処法。purge設定に"html.erb"を含めていないことが原因
+  //https://qiita.com/hajsu00/items/d7f2c7f382124d6e32ef
+  '../myapp/app/views/**/*.{html.erb,erb,haml,html,slim}',
+  '../myapp/app/views/devise/**/*.{html.erb,erb,haml,html,slim}'
 
 
   ],

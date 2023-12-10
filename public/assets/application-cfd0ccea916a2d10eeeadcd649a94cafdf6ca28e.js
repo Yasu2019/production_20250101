3d602f9a2f123a4dcd6414b0990bd663a360bbd7
Yasu2019 <48149403+Yasu2019@users.noreply.github.com>
2023-12-10 10:59:32 +0900

    Remove public/assets from Git

--- public/assets/application-cfd0ccea916a2d10eeeadcd649a94cafdf6ca28e.js
+++
@@ -1,9 +0,0 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-
-//【rails】chartkick導入ガイド
-//https://qiita.com/Yu_unI1/items/42ee303173739c26cf60
-
-import "chartkick"
-import "Chart.bundle"

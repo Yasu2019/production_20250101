708581bea0d45ff787a262df0b147f0a630650de
Yasu2019 <48149403+Yasu2019@users.noreply.github.com>
2024-01-14 22:51:06 +0900

    Revise Supplier test code

--- test/controllers/suppliers_controller_test.rb
+++ test/controllers/suppliers_controller_test.rb
@@ -1,8 +1,11 @@
 require "test_helper"
 
 class SuppliersControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers # Deviseのテストヘルパーをインクルード
+
   setup do
-    @supplier = suppliers(:one)
+    @supplier = suppliers(:one) # suppliers(:one) はテスト用のサプライヤーフィクスチャを指します
+    sign_in users(:one) # users(:one) はテスト用のユーザーフィクスチャを指します
   end
 
   test "should get index" do
@@ -16,8 +19,8 @@ class SuppliersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create supplier" do
-    assert_difference("Supplier.count") do
-      post suppliers_url, params: { supplier: { address1: @supplier.address1, address2: @supplier.address2, automotive_related: @supplier.automotive_related, department: @supplier.department, departments: @supplier.departments, development: @supplier.development, fax: @supplier.fax, iso_existence: @supplier.iso_existence, manufacturer_name: @supplier.manufacturer_name, postal_code: @supplier.postal_code, qms: @supplier.qms, second_party_audit: @supplier.second_party_audit, supplier_development: @supplier.supplier_development, supplier_name: @supplier.supplier_name, target: @supplier.target, tel: @supplier.tel, transaction_details: @supplier.transaction_details } }
+    assert_difference('Supplier.count') do
+      post suppliers_url, params: { supplier: { supplier_name: '新規サプライヤー', manufacturer_name: '新規メーカー', iso_existence: 'ISO9001取得有り', target: true, qms: 'ISO 9001', second_party_audit: '△', supplier_development: '？', automotive_related: true, departments: '加工課', transaction_details: '新規取引詳細', address1: '新規住所1', address2: '新規住所2', postal_code: '123-4567', tel: '03-1234-5678', fax: '03-1234-5679' } }
     end
 
     assert_redirected_to supplier_url(Supplier.last)
@@ -34,12 +37,15 @@ class SuppliersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update supplier" do
-    patch supplier_url(@supplier), params: { supplier: { address1: @supplier.address1, address2: @supplier.address2, automotive_related: @supplier.automotive_related, department: @supplier.department, departments: @supplier.departments, development: @supplier.development, fax: @supplier.fax, iso_existence: @supplier.iso_existence, manufacturer_name: @supplier.manufacturer_name, postal_code: @supplier.postal_code, qms: @supplier.qms, second_party_audit: @supplier.second_party_audit, supplier_development: @supplier.supplier_development, supplier_name: @supplier.supplier_name, target: @supplier.target, tel: @supplier.tel, transaction_details: @supplier.transaction_details } }
+    patch supplier_url(@supplier), params: { supplier: { supplier_name: '更新サプライヤー' } }
     assert_redirected_to supplier_url(@supplier)
+    # データベースの値が更新されたことを確認
+    @supplier.reload
+    assert_equal '更新サプライヤー', @supplier.supplier_name
   end
 
   test "should destroy supplier" do
-    assert_difference("Supplier.count", -1) do
+    assert_difference('Supplier.count', -1) do
       delete supplier_url(@supplier)
     end
 

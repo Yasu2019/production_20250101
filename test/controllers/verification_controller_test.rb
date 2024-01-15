5f0132bd0e68845f1ac1e99e7e5b9b6e48c3325d
Yasu2019 <48149403+Yasu2019@users.noreply.github.com>
2024-01-15 23:06:38 +0900

    Revised

--- test/controllers/verification_controller_test.rb
+++ test/controllers/verification_controller_test.rb
@@ -4,31 +4,26 @@ class VerificationControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
   setup do
-    @user_with_valid_token = users(:valid_token_user) # 有効なトークンを持つユーザー
-    @user_with_expired_token = users(:expired_token_user) # 期限切れのトークンを持つユーザー
-    # テストユーザーをここで作成またはfixturesで定義してください。
+    @user_with_valid_token = users(:valid_token_user)
+    @user_with_expired_token = users(:expired_token_user)
   end
 
-  test "should verify user with valid token" do
-    # 有効なトークンを持つユーザーでverifyアクションをテスト
+  test "verify user with a valid token" do
     get verify_token_path(token: @user_with_valid_token.verification_token)
     assert_redirected_to root_path
   end
 
-  test "should not verify user with expired token" do
-    # 期限切れのトークンを持つユーザーでverifyアクションをテスト
+  test "do not verify user with an expired token" do
     get verify_token_path(token: @user_with_expired_token.verification_token)
     assert_redirected_to new_user_session_path
     follow_redirect!
     assert_equal "トークンが期限切れです。", flash[:alert]
   end
 
-  test "should not verify user with invalid token" do
-    # 存在しないトークンでverifyアクションをテスト
+  test "do not verify user with a non-existent token" do
     get verify_token_path(token: "nonexistenttoken")
     assert_redirected_to new_user_session_path
     follow_redirect!
     assert_equal "トークンが存在しません。", flash[:alert]
   end
 end
-#test

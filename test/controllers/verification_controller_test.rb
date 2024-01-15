bafb7813c09a16d17d1030e87756a3796fb604f6
Yasu2019 <48149403+Yasu2019@users.noreply.github.com>
2024-01-15 17:37:56 +0900

    Revise verification_controller_test.rb

--- test/controllers/verification_controller_test.rb
+++ test/controllers/verification_controller_test.rb
@@ -1,7 +1,33 @@
 require "test_helper"
 
 class VerificationControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user_with_valid_token = users(:valid_token_user) # 有効なトークンを持つユーザー
+    @user_with_expired_token = users(:expired_token_user) # 期限切れのトークンを持つユーザー
+    # テストユーザーをここで作成またはfixturesで定義してください。
+  end
+
+  test "should verify user with valid token" do
+    # 有効なトークンを持つユーザーでverifyアクションをテスト
+    get verify_token_path(token: @user_with_valid_token.verification_token)
+    assert_redirected_to root_path
+  end
+
+  test "should not verify user with expired token" do
+    # 期限切れのトークンを持つユーザーでverifyアクションをテスト
+    get verify_token_path(token: @user_with_expired_token.verification_token)
+    assert_redirected_to new_user_session_path
+    follow_redirect!
+    assert_equal "トークンが期限切れです。", flash[:alert]
+  end
+
+  test "should not verify user with invalid token" do
+    # 存在しないトークンでverifyアクションをテスト
+    get verify_token_path(token: "nonexistenttoken")
+    assert_redirected_to new_user_session_path
+    follow_redirect!
+    assert_equal "トークンが存在しません。", flash[:alert]
+  end
 end
